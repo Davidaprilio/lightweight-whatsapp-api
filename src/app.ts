5db53b01314ec01d958b0b4f78acd77399156954
Davidaprilio <david1.aprilio4@gmail.com>
2022-04-17 23:35:04 +0700

    add  start Client on app starting and call route

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 require("dotenv").config();
 import express from "express";
+import bodyParser from "body-parser";
 import process from "process";
 import mongoose from "mongoose";
 import methodOverride from "method-override";
+import routes from "./routes/api";
+import Device from "./models/Device";
+import { handleStart } from "./controllers/deviceController";
 
 const app = express();
 const port: Number = 3000;
@@ -17,17 +21,31 @@ mongoose
 
 // Method Overide
 app.use(methodOverride("_method"));
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(express.json()); // for parsing application/json
+// app.use(bodyParser.urlencoded({ extended: false })); // for parsing application/x-www-form-urlencoded
+// app.use(bodyParser.json()); // for parsing application/json
+
 process.on("SIGINT", async () => {
   console.log("(SIGINT) Shutting down...");
   // await client.destroy()
   process.exit(0);
 });
 
-app.use(express.json()); // for parsing application/json
-app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+Device.find({ auth: true })
+  .then((devices) => {
+    devices.forEach((device) => {
+      const mode = device.mode != "Legacy";
+      console.log("Start " + device.cid);
+
+      handleStart(device.cid, mode);
+    });
+  })
+  .catch((err) => {
+    console.error("Starting device", err);
+  });
 
 // call Routes
-const routes = require("./routes/api");
 app.use("/api", routes);
 
 app.listen(port, () => {
